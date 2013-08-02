d60bf423cf2d33d81492e7c057d9fad6c688a499
mgmsav <mgmsav@hotmail.com>
2013-08-02 01:24:40 -0400

    the array

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+  
+  $myarray = array('steve', 'joe', 'stan');
+  $myarray[] = 'mark';
+ 
+  print_r($myarray);
+
+  $associative_array = array('name' => 'joe', 'name2' => 'stan');
+   
+  echo count($associative_array); 
+   print_r($associative_array);
+  
+
+?>
