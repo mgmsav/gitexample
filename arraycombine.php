11d4d71df6df90becfb7441da83da82f24f73228
mgmsav <mgmsav@hotmail.com>
2013-08-06 01:57:48 -0400

    Now the first array is the keys the second is the values

--- arraycombine.php
+++ arraycombine.php
@@ -1,7 +1,7 @@
 <?php
-$a = array('green', 'red', 'yellow');
-$b = array('avocado', 'apple', 'banana');
-$c = array($a, $b);
+$a = array('green', 'red', 'yellow','orange');
+$b = array('avocado', 'apple', 'banana','orange');
+$c = array_combine($a, $b);
 
 print_r($c);
 ?>
