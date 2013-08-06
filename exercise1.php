0cd52ed290b466f93591de902b1829ab1f63f937
mgmsav <mgmsav@hotmail.com>
2013-08-06 00:55:34 -0400

    nothing added

--- exercise1.php
+++ exercise1.php
@@ -1,10 +1,11 @@
-<?php
+i<?php
   $account = new account(10000);
   $account->debit(100);
   $account->debit(100);
   $account->credit(200);
   $account->debit(400);
   $account->credit(500);
+  $account->nothing(500);
   $account->credit(1000);
   $account->run();
 //  print_r($account);
