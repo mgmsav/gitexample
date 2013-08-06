f4fb06675abf56125e34c23f4985555d28215cd9
mgmsav <mgmsav@hotmail.com>
2013-08-06 02:41:38 -0400

    pink added

--- arraydiff.php
+++ arraydiff.php
@@ -1,5 +1,5 @@
 <?php
-$array1 = array("a" => "green", "red", "blue", "red");
+$array1 = array("a" => "green", "red", "blue", "red", "pink");
 $array2 = array("b" => "green", "yellow", "red");
 $result = array_diff($array1, $array2);
 
