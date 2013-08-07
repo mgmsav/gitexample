3ac60d3d635b8b24822370f4abb98f30066ea2af
mgmsav <mgmsav@hotmail.com>
2013-08-06 22:06:43 -0400

    new array

---
+++ 0806arry.php
@@ -0,0 +1,9 @@
+<?php
+ $colors = array('first' => 'red', 'second' => 'green');
+ $shapes = array('first' => 'square', 'second' => 'circle');
+ 
+ print_r($colors);
+ echo '<br>';
+ print_r($shapes); 
+
+?>
