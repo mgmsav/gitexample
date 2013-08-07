6130b6eb71092b07f7ea86aa17191c0f0c1dc1c1
mgmsav <mgmsav@hotmail.com>
2013-08-06 23:34:47 -0400

    Merge Combine and Asort

--- 0806arry.php
+++ 0806arry.php
@@ -1,20 +1,33 @@
 <?php
+ function input ($text){
+  echo '<br/>' .'<br/>' .'<u>' . $text . '</u>' . ' <br/>';
+ }
+
  $colors = array('first' => 'red', 'second' => 'green');
  $shapes = array('first' => 'square', 'second' => 'circle');
- 
+ $seasons = array('winter' => 'cold', 'summer' => 'hot');
+ input('printing the colors array  ');
  print_r($colors);
- echo '<br>';
+ input('Printing the shapes array  ');
  print_r($shapes); 
  $combine = array_combine($colors, $shapes);
- echo '<br>';
+ input('Combined the arrays and set colors on the left as the key and shapes on the right as the value');
  print_r($combine);
  
-$merge = array_merge($colors, $shapes);
- echo '<br>';
+ $merge = array_merge($colors, $shapes);
+ input('Merge will over write the parameters that have the same key the second parameter will show ');
  print_r($merge);
 
  $merge = array_merge($colors, $combine);
- echo '<br>';
+ input('Merging will not do anything because no keys are the same');
  print_r($merge);
+ 
+ $new_array = array_merge($seasons, $shapes);
+ input('Merging will not do anything because no keys are the same');
+ print_r($new_array);
+ asort($new_array);
+ input('Asort will sort in alphabetical  order of values');
+ print_r($new_array);
 
 ?>
+
