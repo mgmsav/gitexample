2d35b150336ffe3bb542d9b3a6b8f5383c1c8257
mgmsav <mgmsav@hotmail.com>
2013-08-06 22:41:26 -0400

    combine and merge

--- 0806arry.php
+++ 0806arry.php
@@ -5,5 +5,16 @@
  print_r($colors);
  echo '<br>';
  print_r($shapes); 
+ $combine = array_combine($colors, $shapes);
+ echo '<br>';
+ print_r($combine);
+ 
+$merge = array_merge($colors, $shapes);
+ echo '<br>';
+ print_r($merge);
+
+ $merge = array_merge($colors, $combine);
+ echo '<br>';
+ print_r($merge);
 
 ?>
