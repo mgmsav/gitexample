386dacb7c21955d1b74cf6878cac83cd3599ce0c
mgmsav <mgmsav@hotmail.com>
2013-08-18 21:35:11 -0400

    Email form

---
+++ etg7.php
@@ -0,0 +1,18 @@
+<HTML>
+<HEAD>
+</HEAD>
+<BODY>
+
+<form action="email_script2.php" method="POST">
+
+<p>Email Address: <input type="text" name="email" size="30"></p>
+
+<p>Subject: <input type="text" name="subject" size="30"></p>
+
+<p>Message: </p>
+<p><textarea rows="10" colos="20" name="message"></textarea></p>
+
+<input type="submit" name="submit" value="Submit">
+
+</body>
+
